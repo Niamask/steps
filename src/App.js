87f52a7a95e7143b82fefe3cd1f1b37a3625aa79
Niamask <niamaskay@gmail.com>
2024-03-27 17:18:10 +0000

    Do exercice 2

--- src/App.js
+++ src/App.js
@@ -2,42 +2,109 @@
 
 import { useState } from "react";
 
-// :::::::::::::::::::::::::::::::::::::::::::::::::::: //
-// CHALLENGE #1
-
 export default function App(params) {
-  const [step, setStep] = useState(1);
-  const [count, setCount] = useState(0);
-
-  const date = new Date();
-  console.log(date);
-  date.setDate(date.getDate() + count);
-
   return (
-    <div className="container">
-      <div>
-        <button onClick={() => setStep((c) => c - 1)}>-</button>
-        <span>Step: {step}</span>
-        <button onClick={() => setStep((c) => c + 1)}>+</button>
-      </div>
-      <div>
-        <button onClick={() => setCount((c) => c - step)}>-</button>
-        <span>Count: {count}</span>
-        <button onClick={() => setCount((c) => c + step)}>+</button>
-      </div>
-      <p>
-        <span>
-          {count === 0
-            ? "today is "
-            : count > 0
-            ? `${count}  days from  today is `
-            : `${Math.abs(count)} days ago was `}
-        </span>
-        <span> {date.toDateString()}</span>
-      </p>
+    <div className="App">
+      <FlashCards />
     </div>
   );
 }
+
+function FlashCards() {
+  const [selectedId, setSelectedId] = useState(null);
+  console.log(selectedId);
+  function handleClick(id) {
+    setSelectedId(id !== selectedId ? id : null);
+  }
+
+  return (
+    <div className="flashCards">
+      {questions.map((question) => (
+        <div
+          onClick={() => handleClick(question.id)}
+          className={question.id === selectedId ? "selected" : "item"}
+          key={question.id}
+        >
+          <p>
+            {question.id === selectedId ? question.anwer : question.question}
+          </p>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+const questions = [
+  {
+    id: 3457,
+    question: "What language is React base on?",
+    anwer: "JavaScript",
+  },
+  {
+    id: 7336,
+    question: "What's are the building blocks  of React apps?",
+    anwer: "Components",
+  },
+  {
+    id: 3433,
+    question:
+      "What's the name of the syntax we use to dexcribe a  UI in React ?",
+    anwer: "JavaScript",
+  },
+  {
+    id: 1297,
+    question: "How  to  pass data from parent to child  components?",
+    anwer: "Props",
+  },
+  {
+    id: 3455,
+    question: "How to   give comonents memory?",
+    anwer: "JavaScript",
+  },
+  {
+    id: 2002,
+    question:
+      "What do we call an  input element that is  completely synchronised with  state?",
+    anwer: "Controlled element",
+  },
+];
+
+// :::::::::::::::::::::::::::::::::::::::::::::::::::: //
+// CHALLENGE #1
+
+// export default function App(params) {
+//   const [step, setStep] = useState(1);
+//   const [count, setCount] = useState(0);
+
+//   const date = new Date();
+//   console.log(date);
+//   date.setDate(date.getDate() + count);
+
+//   return (
+//     <div className="container">
+//       <div>
+//         <button onClick={() => setStep((c) => c - 1)}>-</button>
+//         <span>Step: {step}</span>
+//         <button onClick={() => setStep((c) => c + 1)}>+</button>
+//       </div>
+//       <div>
+//         <button onClick={() => setCount((c) => c - step)}>-</button>
+//         <span>Count: {count}</span>
+//         <button onClick={() => setCount((c) => c + step)}>+</button>
+//       </div>
+//       <p>
+//         <span>
+//           {count === 0
+//             ? "today is "
+//             : count > 0
+//             ? `${count}  days from  today is `
+//             : `${Math.abs(count)} days ago was `}
+//         </span>
+//         <span> {date.toDateString()}</span>
+//       </p>
+//     </div>
+//   );
+// }
 
 // export default function App() {
 //   const [step, setStep] = useState(1);
